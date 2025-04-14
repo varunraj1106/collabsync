9a0f9862480b6487cd01b999680018ac9ad26e7e
varunrajkadam <varunraj.kadam@gmail.com>
2025-04-14 02:40:41 +0100

    Initial backend commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,13 @@
+// db.js
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log("✅ Connected to MongoDB Atlas");
   } catch (error) {
     console.error("❌ MongoDB Atlas connection error:", error);
